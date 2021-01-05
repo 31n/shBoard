699e848d0afe19f710109b5662ebb81d0a3ed5bf
31n <41046238+31n@users.noreply.github.com>
2021-01-06 06:37:03 +0900

    - Add Control function

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -8,9 +8,20 @@
 import SwiftUI
 
 struct ContentView: View {
+    let onColor = Color(red: 255 / 255, green: 51 / 255, blue: 153 / 255)
+    let offColor = Color(red: 245 / 255, green: 245 / 255, blue: 245 / 255)
+    let offColor_S = Color(red: 235 / 255, green: 235 / 255, blue: 235 / 255)
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ForEach(1..<15){ i in
+            HStack(spacing: 5){
+                ForEach(1..<19){ j in
+                    RoundedRectangle(cornerRadius: 6)
+                        .fill(offColor)
+                        .frame(width:47, height: 47)
+                }
+            }
+        }
+        .statusBar(hidden: true)
     }
 }
 
