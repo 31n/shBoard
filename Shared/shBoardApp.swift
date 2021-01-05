e7ba2d41b7643e78909395ce333d10257ed7d636
31n <41046238+31n@users.noreply.github.com>
2021-01-05 17:41:56 +0900

    Initial Commit

---
+++ Shared/shBoardApp.swift
@@ -0,0 +1,17 @@
+//
+//  shBoardApp.swift
+//  Shared
+//
+//  Created by Eizo Ishikawa on 2021/01/05.
+//
+
+import SwiftUI
+
+@main
+struct shBoardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
